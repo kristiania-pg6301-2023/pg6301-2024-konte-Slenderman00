4b7564828b82bde1081df8eafb84ea9a8ba271bf
Joar Heimonen <joarheimonen@live.no>
2024-02-20 01:02:32 +0100

    Muy importante forgot D

--- server/src/models/user.ts
+++ server/src/models/user.ts
@@ -11,7 +11,7 @@ const userSchema = new mongoose.Schema(
         },
         hash: {
             type: String,
-            require: true
+            required: true
         },
         uuid: {
             type: String,
