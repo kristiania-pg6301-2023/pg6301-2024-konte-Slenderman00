3a77cdba708ce2068cebfda46f0b4fecdb896020
Joar Heimonen <joarheimonen@live.no>
2024-02-21 06:52:39 +0100

    Removed jwt stuff

--- server/src/secureUserSystem/jwtUtils.ts
+++ server/src/secureUserSystem/jwtUtils.ts
@@ -1,12 +1,12 @@
 const jwt = require('jsonwebtoken');
 
 export function generateToken(uuid: String, role: Number, username: String) {
-    return jwt.sign({ uuid: uuid, role: role, username: username }, process.env.JWT_SECRET || "shhh");
+    return jwt.sign({ uuid: uuid, role: role, username: username }, process.env.JWT_SECRET);
 }
 
 export function verifyToken(_jwt: String) {
     try {
-        var result = jwt.verify(_jwt, process.env.JWT_SECRET || "shhh");
+        var result = jwt.verify(_jwt, process.env.JWT_SECRET);
         return result || false;
     } catch(err) {
         return false;
