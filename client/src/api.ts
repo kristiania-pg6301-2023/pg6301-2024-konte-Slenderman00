f6bfe0ea891bb8c502eaef6ea440dcb2ce96ed7d
Joar Heimonen <joarheimonen@live.no>
2024-02-21 03:46:02 +0100

    Made client able to run locally again

--- client/src/api.ts
+++ client/src/api.ts
@@ -1,4 +1,4 @@
-const ENDPOINT = "https://the-flat-earther-tribune-e93e502a5a83.herokuapp.com";
+const ENDPOINT = import.meta.env.REACT_API || "http://localhost:3000";
 
 function postData(path: string, data: any, callback: any) {
   fetch(`${ENDPOINT}${path}`, {
