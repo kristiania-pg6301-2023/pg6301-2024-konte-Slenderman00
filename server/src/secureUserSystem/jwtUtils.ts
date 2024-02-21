7a9fbf3bad782ba7b2d845376ef05e358f12b4b6
Slenderman00 <Slenderman00@users.noreply.github.com>
2024-02-21 05:53:47 +0000

    Prettified Code!

--- server/src/secureUserSystem/jwtUtils.ts
+++ server/src/secureUserSystem/jwtUtils.ts
@@ -1,14 +1,17 @@
 const jwt = require("jsonwebtoken");
 
 export function generateToken(uuid: String, role: Number, username: String) {
-    return jwt.sign({ uuid: uuid, role: role, username: username }, process.env.JWT_SECRET);
+  return jwt.sign(
+    { uuid: uuid, role: role, username: username },
+    process.env.JWT_SECRET,
+  );
 }
 
 export function verifyToken(_jwt: String) {
-    try {
-        var result = jwt.verify(_jwt, process.env.JWT_SECRET);
-        return result || false;
-    } catch(err) {
-        return false;
-    }
+  try {
+    var result = jwt.verify(_jwt, process.env.JWT_SECRET);
+    return result || false;
+  } catch (err) {
+    return false;
+  }
 }
